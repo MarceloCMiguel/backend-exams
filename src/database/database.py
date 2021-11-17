2978c7e7eb7b7575f4339c23ee847ee1c562ff11
MarceloCMiguel <marcelocmiguel@outlook.com>
2021-11-17 19:46:03 -0300

    added database enviroments to git ignore

--- src/database/database.py
+++ src/database/database.py
@@ -1,16 +1,22 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-# from sqlalchemy_utils import create_database, database_exists
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:123456@localhost:3306/exams"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+from dotenv import load_dotenv
 
+import os
+load_dotenv() 
+ 
+USER_MYSQL = os.environ.get("USER_MYSQL") 
+PASSWORD_MYSQL = os.environ.get('PASSWORD_MYSQL') 
+SERVER_MYSQL = os.environ.get('SERVER_MYSQL') 
+DATABASE_MYSQL = os.environ.get('DATABASE_MYSQL') 
+# from sqlalchemy_utils import create_database, database_exists
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{USER_MYSQL}:{PASSWORD_MYSQL}@{SERVER_MYSQL}/{DATABASE_MYSQL}"
+print("-"*100)
+print(SQLALCHEMY_DATABASE_URL)
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
-
-# if not database_exists(engine.url):
-#     create_database(engine.url)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
